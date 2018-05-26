503c2f4141567bfde1961d53a50c58bbb749c99a
YangJiaQi <1295585769@qq.com>
2018-05-26 10:24:24 +0800

    01

---
+++ Assets/Scripts/DrawTheCrosshair.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawTheCrosshair : MonoBehaviour
+{
+
+    public Texture texture;
+    /// <summary>  
+    /// 绘制准心  
+    /// </summary>  
+    void OnGUI()
+    {
+        //texture.width >> 1和(texture.height >>是右移一位，  
+        //相当于除以2。(x >> 1) 和 (x / 2) 的结果是一样的。  
+        //创建一个新的矩形  
+        Rect rect = new Rect(Input.mousePosition.x - (texture.width >> 1),//矩形的X轴坐标  
+            Screen.height - Input.mousePosition.y - (texture.height >> 1),//矩形的y轴的坐标  
+            texture.width,//矩形的宽  
+            texture.height);//矩形的高  
+        GUI.DrawTexture(rect, texture);//开始绘制 
+    }
+}
---
+++ Assets/Test.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	/// <summary>
+    /// 测试哈哈哈
+    /// 
+    /// </summary>
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
